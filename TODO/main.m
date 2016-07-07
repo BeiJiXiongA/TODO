c7e9c95d8afda90823f45182ab92aecc0bbabf2a
ZhangWei-SpaceHome <ZW_SapceHome@173-16-22-223.client.mchsi.com>
2016-07-07 17:48:39 +0800

    Initial Commit

---
+++ TODO/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TODO
+//
+//  Created by ZhangWei-SpaceHome on 16/7/7.
+//  Copyright © 2016年 zhangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
